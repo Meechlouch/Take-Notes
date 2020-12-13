f50b59a92056fbb8c2ac4fb18b078d403d15039b
Dj Meechlouch <Meechlouch@gmail.com>
2020-12-12 23:29:04 -0600

    routes created for index.html and notes.html

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+//Setup routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 3000;
 
