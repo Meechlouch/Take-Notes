055b130e9a2eeb9de52f09c8454e5988348c7a30
Dj Meechlouch <Meechlouch@gmail.com>
2020-12-15 09:50:57 -0600

    successfully sent POST Request to server and saved notes with an id.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,23 @@
-const { json } = require("body-parser");
-const express = require("express");
 const fs = require("fs");
-const path = require("path");
+//const path = require("path");
 const dataBase = require("../db/db.json");
-
+const data = "./db/db.json";
 module.exports = function (app) {
   // Setup express app to listen to a 'GET' request on route /api/notes
-  app.get("/api/notes", function (req, res) {
-    res.json(dataBase);
+  app.get("/api/notes", (req, res) => {
+    return res.json(dataBase);
+  });
+
+  app.post("/api/notes", (req, res) => {
+    let savedData = fs.readFileSync(data, "utf8");
+    //console.log(savedData);
+    savedData = JSON.parse(savedData);
+    //console.log(savedData);
+    req.body.id = savedData.length;
+    savedData.push(req.body);
+    //console.log(savedData);
+    savedData = JSON.stringify(savedData);
+    fs.writeFileSync(data, savedData);
+    console.log(savedData);
   });
 };
