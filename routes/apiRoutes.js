0cae07c719f973a60df53304f630c6e012bd2394
Dj Meechlouch <Meechlouch@gmail.com>
2020-12-16 20:20:19 -0600

    page is rendering without refresh

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,29 +1,35 @@
 const fs = require("fs");
 const dataBase = require("../db/db.json");
 const data = "./db/db.json";
+
 module.exports = function (app) {
   // Setup express app to listen to a 'GET' request on route /api/notes
   app.get("/api/notes", (req, res) => {
-    return res.json(dataBase);
+    fs.readFile(data, (err, resp) => {
+      if (err) throw err;
+      res.json(JSON.parse(resp));
+    });
   });
 
   app.post("/api/notes/", (req, res) => {
     let savedData = fs.readFileSync(data, "utf8");
     savedData = JSON.parse(savedData);
     savedData.push(req.body);
-
+    console.log(req.body);
     for (let i = 0; i < savedData.length; i++) {
       savedData[i].id = i;
     }
 
     savedData = JSON.stringify(savedData);
     fs.writeFileSync(data, savedData);
-    res.send(savedData);
+    res.json(JSON.parse(savedData));
   });
 
   app.delete("/api/notes/:id", (req, res) => {
     let savedData = fs.readFileSync(data, "utf8");
+    console.log(savedData);
     savedData = JSON.parse(savedData);
+    console.log(savedData);
     let idNum = parseInt(req.params.id);
     //oneNote = savedData.filter((savedData) => savedData.id === idNum);
     console.log(idNum);
@@ -32,8 +38,10 @@ module.exports = function (app) {
     for (let i = 0; i < savedData.length; i++) {
       savedData[i].id = i;
     }
+    console.log(savedData);
     savedData = JSON.stringify(savedData);
+    console.log(savedData);
     fs.writeFileSync(data, savedData);
-    res.send(savedData);
+    res.send(JSON.parse(savedData));
   });
 };
