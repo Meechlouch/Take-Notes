57539b2f8151c2664a3ffffa41c219a8f9dcd3ec
Dj Meechlouch <Meechlouch@gmail.com>
2020-12-17 15:38:41 -0600

    comments added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,37 +17,43 @@ module.exports = function (app) {
 
   // Setup express app to listen for a 'POST' request on route /api/notes
   app.post("/api/notes/", (req, res) => {
+    // fs reads a file at data path location, then saves data to variable
     let savedData = fs.readFileSync(data, "utf8");
+    // Data recieved is parsed to a json object
     savedData = JSON.parse(savedData);
+    // Data is then pushed to the body of the 'POST' request
     savedData.push(req.body);
-    console.log(req.body);
-
+    // for loop adds an id to every index of our array
     for (let i = 0; i < savedData.length; i++) {
       savedData[i].id = i + 1;
     }
-
-    console.log(savedData);
+    // The modified array is then stringified before being written to file
     savedData = JSON.stringify(savedData);
+    // The modified array is then written back to file
     fs.writeFileSync(data, savedData);
+    // Modified data is then sent to client as a JSON object
     res.json(JSON.parse(savedData));
   });
 
+  // Setup app to listen for 'delete' request on route api/notes/ with a param id.
   app.delete("/api/notes/:id", (req, res) => {
+    // fs reads a file at data path location, then saves data to variable
     let savedData = fs.readFileSync(data, "utf8");
-    console.log(savedData);
+    // The data is then parsed from a string to a JSON object
     savedData = JSON.parse(savedData);
-
+    // Our param.id is parsed from a string to a number and saved to variable
     let idNum = parseInt(req.params.id);
-    //oneNote = savedData.filter((savedData) => savedData.id === idNum);
-    console.log(idNum);
-
+    // delete from array
     savedData.splice(idNum - 1, 1);
+    // reset id at every index
     for (let i = 0; i < savedData.length; i++) {
       savedData[i].id = i + 1;
     }
-    console.log(savedData);
+    // The modified array is then stringified before being written to file
     savedData = JSON.stringify(savedData);
+    // The modified array is then written back to file
     fs.writeFileSync(data, savedData);
+    // Modified data is then sent to client as a JSON object
     res.send(JSON.parse(savedData));
   });
 };
